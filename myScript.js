b110d708e76de1f8b6d7485237bba2387ee0e40e
Schlebbe <sebastian@SCHLEBBE-PC>
2019-03-07 09:32:26 +0100

    JS and html base

---
+++ myScript.js
@@ -0,0 +1,12 @@
+let button = document.getElementById("currencyConvert");
+button.addEventListener("click", convert);
+
+
+
+function convert() {
+    let currencyInput = document.getElementById("currencyInput").value;
+    let currencyFrom = document.getElementById("currencyFrom").value;
+    let currencyTo = document.getElementById("currencyTo").value;
+
+
+}
