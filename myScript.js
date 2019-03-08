b826172b33fda75bc2f1a512abb1c4b8ef8cc42b
Schlebbe <sebastian@SCHLEBBE-PC>
2019-03-08 09:28:21 +0100

    Added conversion calculation

--- myScript.js
+++ myScript.js
@@ -1,11 +1,13 @@
 let button = document.getElementById("currencyConvert");
 button.addEventListener("click", Convert);
 
+// let globalResultLmao = 
 GetData();
+var result;
 
 async function GetData() {
     let url = "https://openexchangerates.org/api/latest.json?app_id=548eceb07c57435397f105655e9f882c";
-    let result = await FetchData(url);
+    result = await FetchData(url);
     let currencies = document.querySelectorAll(".currency");
 
     for (const property in result.rates) {
@@ -31,6 +33,7 @@ async function GetData() {
             }
         }
     }
+    return result;
 }
 
 async function FetchData(url) {
@@ -44,10 +47,39 @@ function Convert() {
     let currencyFrom = document.getElementById("currencyFrom").value;
     let currencyTo = document.getElementById("currencyTo").value;
     let buyOrSell = document.getElementsByName("buyOrSell");
+    //let newValue = wjfhiuhf
 
     for (const radio of buyOrSell) {
         if (radio.checked) {
             buyOrSell = radio.value; //True = buy, false = sell.
         }
     }
+
+    for (const property in result.rates) {
+        if (result.rates.hasOwnProperty(property)) {
+            const currencyFromValue = result.rates[property];
+
+            if (property == currencyFrom) {
+                // alert(currencyFromValue); //formula = x/firstrate*2ndrate
+
+                for (const key in result.rates) {
+                    if (result.rates.hasOwnProperty(key)) {
+                        const currencyToValue = result.rates[key];
+                        if (currencyInput != null && key == currencyTo) {
+                            newValue = currencyInput / currencyFromValue * currencyToValue;
+                            alert(`From ${currencyInput} ${property} to ${key}: ${newValue}`);
+
+                        }
+
+                    }
+                }
+            }
+            // alert(element);
+        }
+    }
+    // if (condition) {
+
+    // }
+    // let test = 
+
 }
